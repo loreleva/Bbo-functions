f17c093b6c15160d22cbe1794a1a04b0e774a164
Lorenzo Levantesi <lorenzo2609@hotmail.it>
2022-11-23 13:14:31 +0100

    fixed json ackley_function typo

--- sfu/evaluation_code/main.py
+++ sfu/evaluation_code/main.py
@@ -4,8 +4,8 @@ from functions import *
 if __name__ == "__main__":
 	load_json("../functions/functions.json")
 	print("Functions with dimension d:{}\n".format(search_function({"dimension" : "d"})))
-	function_selected = "trid_function"
-	function_dimension = 5
+	function_selected = "ackley_function"
+	function_dimension = 2
 	select_function(function_selected)
 	print("Function selected: {}".format(function_selected))
 	print("Parameters of {}: {}".format(function_selected, parameters()))
