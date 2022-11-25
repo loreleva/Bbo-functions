671b49a114a0db9e7676ede629171e35608a12d0
Lorenzo Levantesi <lorenzo2609@hotmail.it>
2022-11-25 15:53:50 +0100

    Speeded up R code evaluation

--- sfu/evaluation_code/main.py
+++ sfu/evaluation_code/main.py
@@ -1,16 +1,20 @@
-import sys
+import sys, time
 from functions import *
 
 if __name__ == "__main__":
 	load_json("../functions/functions.json")
 	print("Functions with dimension d:{}\n".format(search_function({"dimension" : "d"})))
-	function_selected = "ackley_function"
-	function_dimension = 2
+	function_selected = "trid_function"
+	function_dimension = 5
 	select_function(function_selected)
 	print("Function selected: {}".format(function_selected))
 	print("Parameters of {}: {}".format(function_selected, parameters()))
 	print("Dimension of {}: {}".format(function_selected, dimension()))
 	print("Input domain of {} with dimension {}: {}".format(function_selected, function_dimension ,input_domain(function_dimension)))
 	input_x = [0.5, 0.5, 0.5, 0.5, 0.5]
-	print("{} value of dimension {} at point {}: {}".format(function_selected, function_dimension, input_x, evaluate(input_x)))
+	#evaluate_test(input_x)
+	start_time = time.time()
+	for x in range(50):
+		print("{} value of dimension {} at point {}: {}".format(function_selected, function_dimension, input_x, evaluate_test(input_x)))
+	print(f"TIME: {time.time() - start_time}")
 	
