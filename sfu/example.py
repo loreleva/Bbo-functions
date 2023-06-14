6fdcb47d7b9f2c9c987af30c4fe1e7ff49e1d09d
Lorenzo Levantesi <lorenzo2609@hotmail.it>
2023-06-14 19:20:08 +0200

    Modified names of functions in json file

--- sfu/example.py
+++ sfu/example.py
@@ -1,6 +1,9 @@
 from objective_function_class import *
 
 if __name__ == "__main__":
-	function_selected = "michalewicz_function"
-	dim = 4
-	print(search_functions(filters={"dimension":"d", "minimum_f":True}))
+	function_selected = "rastrigin_function"
+	dim = 10
+	obj = objective_function(function_selected, dim)
+	#print(obj.input_opt)
+	print(obj.evaluate(obj.input_opt))
+	print(obj.opt)
