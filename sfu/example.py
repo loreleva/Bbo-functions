c9ad10f75843e69b161f0e0219d06ff531835357
Lorenzo Levantesi <lorenzo2609@hotmail.it>
2023-06-19 11:20:37 +0200

    Fixed typo in json file for six hump camel function

--- sfu/example.py
+++ sfu/example.py
@@ -1,9 +1,10 @@
 from objective_function_class import *
 
 if __name__ == "__main__":
-	function_selected = "rastrigin_function"
-	dim = 10
+	function_selected = "shekel_function"
+	dim = 4
 	obj = objective_function(function_selected, dim)
 	#print(obj.input_opt)
-	print(obj.evaluate(obj.input_opt))
+	#print(obj.evaluate(obj.input_opt))
 	print(obj.opt)
+	print(obj.evaluate(obj.input_opt))
